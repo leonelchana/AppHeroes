de6d5fd14f2b07b7cbc2c46a8b3f20334381a274
sistemasfinorleonel <96667421+sistemasfinorleonel@users.noreply.github.com>
2023-01-17 13:58:08 -0400

    segundo commit

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from './auth-routing.module';
+
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegistroComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
